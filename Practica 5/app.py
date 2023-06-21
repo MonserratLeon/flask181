533ee6b6c35c3f9173c05c0065a3d5a039d84204
Andres0319 <121038655@upq.edu.mx>
2023-06-21 17:11:48 -0600

    practica 6, insert, redireccionamos, enviamos msj vista

--- Practica 5/app.py
+++ Practica 5/app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+#importacion del framework
+from flask import Flask,render_template,request,redirect,url_for
+from flask_mysqldb import MySQL
 
-app= Flask (__name__)
 
+#inicializacion del APP
+app= Flask(__name__)
+
+#Configuracion de la conexion
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='dbflask'
+mysql= MySQL(app)
+
+#declaracion de ruta http://localhost:5000
 @app.route('/')
 def index():
-    return "Hola Mundo FLASK"
 
+    return render_template('index.html')
+
+#ruta http:localhost:5000/guardar tipo POST para Insert
+@app.route('/guardar',methods=['POST'])
+def guardar():
+    if request.method == 'POST':
+
+        # pasamos a variables el contenido de los input
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['txtArtista']
+        Vanio=request.form['txtAnio']
+        # print(titulo,artista,anio)
+
+# conectar y ejecutar el insert
+
+CS=mysql.connection.cursor()
+CS.execute('insert into tbalbums(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+mysql.connection.commit()
+
+return redirect(url_for'index')
+
+@app.route('/eliminar')
+def eliminar():
+    return "Se elimino en la base de datos"
+
+
+#ejecucion del servidor en el puerto 5000
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
--- Practica 5/conexion.py
+++ Practica 5/conexion.py
@@ -1,41 +1,43 @@
 #importacion del framework
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request,redirect,url_for
 from flask_mysqldb import MySQL
 
-
-#inicializacion del APP
-app= Flask(__name__)
-
-#Configuracion de la conexion
+#Inicializacion del APP
+app= Flask (__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='dbflask'
+app.secret_key= 'mysecretkey'
 mysql= MySQL(app)
 
-#declaracion de ruta http://localhost:5000
+#Declaracion de ruta http://localhost:5000
 @app.route('/')
 def index():
-    return render_template('index.html')
+  
+  return render_template('index.html')
 
-#ruta http:localhost:5000/guardar tipo POST para Insert
+#rura http:localhost:5000/guardar tipo POST para insert
 @app.route('/guardar',methods=['POST'])
 def guardar():
-    if request.method == 'POST':
-        titulo= request.form['txtTitulo']
-        artista= request.form['txtArtista']
-        anio= request.form['txtAnio']
-        print(titulo,artista,anio)
+  if request.method == 'POST':
 
-    return 'los datos llegaron Amigo ;'
+    # pasamos a variables el contenido de los input
+    Vtitulo=request.form['txtTitulo']
+    Vartista=request.form['txtArtista']
+    Vanio=request.form['txtAnio']
+   
+    #Conectar y ejecutar el insert
+    cs= mysql.connection.cursor()
+    cs.execute('insert into tbalbums (titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+    mysql.connection.commit()
 
-    return "se guardo en la base de datos"
+  return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
-    return "Se elimino en la base de datos"
+  return "Se elimino en la BD"
 
-
-#ejecucion del servidor en el puerto 5000
-if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+#Ejecucion del servidor en el puerto 5000
+if __name__ == '_main_':
+  app.run(port=5000,debug=True)
