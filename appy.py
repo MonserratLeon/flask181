fb10463d59aa541c231d7867f805114882613820
MonserratLeon <89002332+MonserratLeon@users.noreply.github.com>
2023-06-26 15:27:31 -0600

    conexion bdexpo

---
+++ appy.py
@@ -0,0 +1,25 @@
+#importacion del framework
+from flask import Flask
+from flask_mysqldb import MySQL
+
+
+#inicializacion del APP
+app= Flask(__name__)
+
+
+#Configuracion de la conexion
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='dbexpo'
+mysql= MySQL(app)
+
+
+#declaracion de ruta http://localhost:5000
+    @app.route('/expo')
+def expo():
+    return render_template('Exploracion y diagnostico 2.html')
+
+#ejecucion del servidor en el puerto 5000
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
